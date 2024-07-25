949411fc0c7a93a996cede0270bdfc7cf907a451
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 12:39:21 +0200

    add outputs

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,3 +1,9 @@
-output "instance_name" {
-  value = google_compute_instance.vm_instance.name
+output "instance_names" {
+  description = "Names of the instances"
+  value = { for k, v in google_compute_instance.vm_instance : k => v.name }
+}
+
+output "instance_ips" {
+  description = "Public IP addresses of the instances"
+  value = { for k, v in google_compute_instance.vm_instance : k => v.network_interface[0].access_config[0].nat_ip }
 }
