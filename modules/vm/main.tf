3535696ef5ef98e5e98d4eaa8039e290e1d5210b
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 11:21:51 +0200

    correct sintaxis

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,8 +1,9 @@
 resource "google_compute_instance" "vm_instance" {
-  for_each    = { for idx, instance in var.instances : idx => instance }
-  name        = each.value.name
+  for_each    = { for idx, vm in var.instances : idx => vm }
+  name        = "${each.value.name}-${each.key}"
   machine_type = each.value.machine_type
   zone        = each.value.zone
+  project     = var.project_id
 
   boot_disk {
     initialize_params {
@@ -12,11 +13,14 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     network = var.network_name
-    access_config {
-    }
   }
 
-  labels = var.labels
-
-  tags = ["${each.value.name}-tag"]
+  labels = merge(
+    var.labels,
+    {
+      "name"       = each.value.name
+      "environment" = var.environment
+      "department"  = var.department
+    }
+  )
 }
