500d4399853ec9456902254e77427a68a9cafd9e
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 11:32:22 +0200

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,6 +1,41 @@
+variable "project_id" {
+  description = "The GCP project to use"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy resources"
+  type        = string
+}
+
+variable "network_name" {
+  description = "The name of the network to deploy resources"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment label for the instances"
+  type        = string
+}
+
+variable "department" {
+  description = "Department label for the instances"
+  type        = string
+}
+
+variable "instances" {
+  description = "A list of instances to create"
+  type        = list(object({
+    name         = string
+    machine_type = string
+    zone         = string
+    image        = string
+  }))
+}
+
 resource "google_compute_instance" "vm_instance" {
   for_each    = { for idx, vm in var.instances : idx => vm }
-  name        = "${each.value.name}-${each.key}"
+  name        = each.value.name
   machine_type = each.value.machine_type
   zone        = each.value.zone
   project     = var.project_id
@@ -15,12 +50,8 @@ resource "google_compute_instance" "vm_instance" {
     network = var.network_name
   }
 
-  labels = merge(
-    var.labels,
-    {
-      "name"       = each.value.name
-      "environment" = var.environment
-      "department"  = var.department
-    }
-  )
+  labels = {
+    environment = var.environment
+    department  = var.department
+  }
 }
