e49fee6bc5074063da6a50b853fa8677f7228f21
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 12:01:39 +0200

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,19 +1,5 @@
-variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-}
-
 variable "network_name" {
   description = "The name of the VPC network"
   type        = string
 }
 
-variable "environment" {
-  description = "Environment label for the instances"
-  type        = string
-}
-
-variable "department" {
-  description = "Department label for the instances"
-  type        = string
-}
