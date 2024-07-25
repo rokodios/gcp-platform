942c3720b2032a64afee26c44fa6496b79b57456
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 10:58:11 +0200

    Update variables to each

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,25 +1,29 @@
 variable "project_id" {
-  description = "The GCP project ID"
+  description = "The GCP project to use"
   type        = string
 }
 
 variable "region" {
-  description = "The GCP region"
-  type        = string
-}
-
-variable "instance_name" {
-  description = "The name of the VM instance"
+  description = "The region to deploy resources"
   type        = string
 }
 
 variable "network_name" {
-  description = "The name of the VPC network"
+  description = "The name of the network to attach the instances"
   type        = string
 }
 
 variable "labels" {
-  description = "A map of labels to assign to the resource"
+  description = "Labels to apply to the instances"
   type        = map(string)
-  default     = {}
+}
+
+variable "instances" {
+  description = "A list of instances to create"
+  type        = list(object({
+    name         = string
+    machine_type = string
+    zone         = string
+    image        = string
+  }))
 }
