66bb048b0083f7137d4d4affaa9e603116e579e3
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 11:36:59 +0200

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,38 +1,3 @@
-variable "project_id" {
-  description = "The GCP project to use"
-  type        = string
-}
-
-variable "region" {
-  description = "The region to deploy resources"
-  type        = string
-}
-
-variable "network_name" {
-  description = "The name of the network to deploy resources"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment label for the instances"
-  type        = string
-}
-
-variable "department" {
-  description = "Department label for the instances"
-  type        = string
-}
-
-variable "instances" {
-  description = "A list of instances to create"
-  type        = list(object({
-    name         = string
-    machine_type = string
-    zone         = string
-    image        = string
-  }))
-}
-
 resource "google_compute_instance" "vm_instance" {
   for_each    = { for idx, vm in var.instances : idx => vm }
   name        = each.value.name
