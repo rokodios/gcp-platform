1fe0b7cee6d59faf10dd62adc97a7af065bec7d0
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 10:58:49 +0200

    Update variables to each

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,14 +1,37 @@
-variable "project" {
-  description = "The GCP project ID"
+variable "project_id" {
+  description = "The GCP project to use"
   type        = string
 }
 
 variable "region" {
-  description = "The GCP region"
+  description = "The region to deploy resources"
   type        = string
 }
 
-variable "env" {
-  description = "The environment name (dev, pre, pro, shared)"
+variable "environment" {
+  description = "Environment label for the instances"
   type        = string
+}
+
+variable "department" {
+  description = "Department label for the instances"
+  type        = string
+}
+
+variable "instances" {
+  description = "A list of instances to create"
+  type        = list(object({
+    name         = string
+    machine_type = string
+    zone         = string
+    image        = string
+  }))
+  default = [
+    {
+      name         = "dev-vm-test"
+      machine_type = "f1-micro"
+      zone         = "us-east1"
+      image        = "debian-cloud/debian-10"
+    }
+  ]
 }
