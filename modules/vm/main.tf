39371d200dc34a003ebf1371f9a1368086450bd0
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 10:50:09 +0200

    Update module vm

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,18 +1,22 @@
 resource "google_compute_instance" "vm_instance" {
-  name         = var.instance_name
-  machine_type = "n1-standard-1"
-  zone         = "${var.region}-a"
-
-  labels = var.labels
+  for_each    = { for idx, instance in var.instances : idx => instance }
+  name        = each.value.name
+  machine_type = each.value.machine_type
+  zone        = each.value.zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-10"
+      image = each.value.image
     }
   }
 
   network_interface {
     network = var.network_name
-    access_config {}
+    access_config {
+    }
   }
+
+  labels = var.labels
+
+  tags = ["${each.value.name}-tag"]
 }
