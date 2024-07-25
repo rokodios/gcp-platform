3e5babad4ad92b31f06b563f5e8258c9bc1c60d5
rokodios <134306990+rokodios@users.noreply.github.com>
2024-07-25 13:12:47 +0200

    delete

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,9 +1,1 @@
-output "instance_names" {
-  description = "Names of the instances"
-  value = { for k, v in google_compute_instance.vm_instance : k => v.name }
-}
 
-output "instance_ips" {
-  description = "Public IP addresses of the instances"
-  value = { for k, v in google_compute_instance.vm_instance : k => v.network_interface[0].access_config[0].nat_ip }
-}
